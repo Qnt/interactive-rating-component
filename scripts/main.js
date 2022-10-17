a327189a7c763623aabadf94f996d51be3ba6701
Qnt <lihodeikvint@gmail.com>
2022-10-18 00:52:26 +0300

    Files added

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let form = document.querySelector('.rating-scale-form');
+let ratingState = document.querySelector('.interactive-component-container');
+let thankYouState = document.querySelector('.thank-you-state-container');
+let ratingValue = document.querySelector('.thank-you-state-rating-value');
+
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+  const ratingData = serializeForm(form);
+  changeDisplay(ratingData);  
+}
+
+const serializeForm = (formNode) => {
+  const { elements } = formNode;
+  const ratingData = Array.from(elements)
+    .filter((item) => !!item.name)
+    .map((element) => {
+      const { name, value, checked } = element;
+      return { name, value, checked };
+  })
+  
+  return ratingData;
+}
+
+const changeDisplay = (data) => {
+  data.map((element) => {
+    if(element.checked) {
+      ratingState.style.display = 'none';
+      thankYouState.style.display = 'flex';
+      ratingValue.innerHTML = `You selected ${element.value} out of 5`
+    }
+  })
+}
+
+form.addEventListener('submit', handleFormSubmit);
